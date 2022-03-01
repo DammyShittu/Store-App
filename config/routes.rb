aebf65428314a54e77e5d49d6baef55d5a7619e4
Shittu Adedamola <shittuadedamola@rocketmail.com>
2022-03-01 16:55:26 +0100

    Add Routes and Seed Data

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :products
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product1 = Product.create(code: "mug", name: "Reedsy Mug", price: 7.30)
+product2 = Product.create(code: "t-shirt", name: "Reedsy T-shirt", price: 14)
+
+ProductShoppingCart.create(quantity: 4, product_id: product1.id, shopping_cart_id: 1)
+ProductShoppingCart.create(quantity: 6, product_id: product2.id, shopping_cart_id: 2)
