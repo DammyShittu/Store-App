c47838742f3975a63e5a95b18b071fa97a38edcd
Shittu Adedamola <shittuadedamola@rocketmail.com>
2022-03-02 09:23:48 +0100

    Implement Current Cart Method In Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  before_action :current_cart
+
+  private
+    def current_cart
+      if session[:shopping_cart_id]
+        cart = ShoppingCart.find_by(:id => session[:shopping_cart_id])
+        if cart.present?
+          @current_cart = cart
+        else
+          session[:shopping_cart_id] = nil
+        end
+      end
+
+      if session[:shopping_cart_id] == nil
+        @current_cart = ShoppingCart.create
+        session[:shopping_cart_id] = @current_cart.id
+      end
+    end
 end
