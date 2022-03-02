9c595f1689e936af1dd74bb2cad037af12b5dbbd
Shittu Adedamola <shittuadedamola@rocketmail.com>
2022-03-02 09:25:11 +0100

    Implement Total Item And Sub Total Logic In Models

--- app/models/product_shopping_cart.rb
+++ app/models/product_shopping_cart.rb
@@ -1,4 +1,8 @@
 class ProductShoppingCart < ApplicationRecord
   belongs_to :product
   belongs_to :shopping_cart
+
+  def total_price
+    self.quantity * self.product.price
+  end
 end
--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,4 +1,12 @@
 class ShoppingCart < ApplicationRecord
   has_many :product_shopping_carts, dependent: :destroy
   has_many :products, through: :product_shopping_carts
+
+  def all_items_total
+    total = 0
+    self.product_shopping_carts.each do |item|
+      total += item.total_price
+    end
+    total
+  end
 end
