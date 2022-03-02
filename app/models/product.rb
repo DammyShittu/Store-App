61913c69e162722826766dae0344a593dc2be3f6
Shittu Adedamola <shittuadedamola@rocketmail.com>
2022-03-02 09:22:49 +0100

    Implement Associations In Models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  before_save :code_uppercase
+  has_many :product_shopping_carts, dependent: :destroy
+
+  private
+
+  def code_uppercase
+    self.code.upcase!
+  end
 end
--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,2 +1,4 @@
 class ShoppingCart < ApplicationRecord
+  has_many :product_shopping_carts, dependent: :destroy
+  has_many :products, through: :product_shopping_carts
 end
