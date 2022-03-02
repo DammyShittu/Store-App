478f07b0ec1e6d68b9c66af682f35ac0a06252ca
Shittu Adedamola <shittuadedamola@rocketmail.com>
2022-03-02 09:26:56 +0100

    Add Extra Route Actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :products
+      resources :shopping_carts, only: [:index, :destroy]
+      resources :product_shopping_carts, only: :create
     end
   end
 end
